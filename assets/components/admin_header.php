58961eba58be5ed293a3b461231bffde45d3e6dd
Kai Lau <kailau112@gmail.com>
2023-11-30 13:20:45 +0000

    Admin login system complete with UI.

--- assets/components/admin_header.php
+++ assets/components/admin_header.php
@@ -8,13 +8,40 @@
 </head>
 
 <body>
-    <?php include "../dig5127_coursework/db/db_connection.php"?>
+
     <nav class="navbar navbar-expand-lg" id="navbar">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"><img src="../assets/images/logo.png" height="36px" alt="LIKEA logo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+            session_start();
+
+            if (isset($_SESSION['username']) && !empty($_SESSION['username']) && basename($_SERVER['PHP_SELF']) == 'admin_index.php') {
+                ?>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./create.php">Create</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./read.php">Read</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./update.php">Update</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="./delete.php">Delete</a>
+                        </li>
+                    </ul>
+                </div>
+                <?php
+            }
+            ?>
         </div>
     </nav>
-
 </body>
 
 </html>
--- db/db_connection.php
+++ db/db_connection.php
@@ -4,11 +4,9 @@ $username = 'root';
 $password = '';
 $database = 'coursework_dig5127';
 
-// Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+} else 
 ?>
