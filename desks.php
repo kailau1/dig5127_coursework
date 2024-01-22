deebbea0240d0cc844f85671eeb06e61b67715a6
Daniel <116022799+danbash14@users.noreply.github.com>
2024-01-22 17:10:40 +0000

    Update desks.php
    Added desk images and updated title

--- desks.php
+++ desks.php
@@ -8,10 +8,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">    <link rel="stylesheet" href="./css/styles.scss">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
-    <title>Products</title>
-</head>
+    <title>Desks</title>
+</head> 
+
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <?php 
         include "./assets/components/header.php";
         include "./db/db_connection.php";
@@ -19,7 +20,7 @@
     <div class="container-flex">
         <div class="row">
             <div class="col-md-12">
-                <h1>Products</h1>
+                <h1>Desks</h1>
             </div>
         </div>
         <div class="row">
@@ -30,4 +31,5 @@
         </div>  
     </div>
 </body>
+</html>
 </html>
