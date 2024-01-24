b64b29fbc606ebbf08ef4961c789c365daf45ac8
Mclarke66 <malachiclarke06@gmail.com>
2024-01-24 12:50:05 +0000

    footer

---
+++ assets/components/Footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    
+    <div class="footer">
+        <p>Kai Lou, Malachi Clarke, Daniel Bashford</p>
+        <p><a href="./admin/admin_login.php">Admin Login</a></p>
+    </div>
+</body>
+</html>
+
+
+
+
